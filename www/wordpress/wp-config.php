e70431b460b45386f73d8fc3f529a28eb3b2c32e
On The Way Treding Co.,Ltd <alang@onlylang.com>
2022-06-21 16:59:32 +0000

    wordpress

---
+++ www/wordpress/wp-config.php
@@ -0,0 +1,33 @@
+
+define('AUTH_KEY',         '<:S}v`=12t5xs8U-L[tg9.WJRd}YmSjo0 ddFYl~SJu*pxm6zh|g+a|eP-X}EfXE');
+define('SECURE_AUTH_KEY',  'C^[SWfq)#h#MCRta;P)2Z;E(}K5sp]4B_XC|<xve Osw2r?3XD`K|{UmI&%5+s6X');
+define('LOGGED_IN_KEY',    '.|y:@zdO_r4,;W0^gvg1BRV;gVKK3%f~.1;?wz| Zk{b;d+a=m<&hpG;p;qet#Jp');
+define('NONCE_KEY',        'U$N(Y;_uB4*wgvFY(Ov[W#r9X zn6l5,|ok38j MfL6rlu])6[9{O$G+~j)bft$[');
+define('AUTH_SALT',        'j$L<A8C%R,5PBraF5AZ3l.bqfZ85]?nR-+H}$-(ErV%H`@7ez@.IQbZBDcVcM+:;');
+define('SECURE_AUTH_SALT', 'c*EYr9eV]Z9,ax9;SQO#0JEeM+[ro|^fw{CW41B`!:WCxvK^neetoIgpIa3l>p.y');
+define('LOGGED_IN_SALT',   'QCI?!~UC!Xx_-`(Oze%[+/&P#_6ecX|$f|~!<.YV2MC,$,KpEkHhm:QB+aq6P|l>');
+define('NONCE_SALT',       'fhBHJ<%YHQ|3:K.L3~+7ye`4c5+v]!$H4ZcLJnDkbF}$`y5h,g-M^Q-jCAG&|dWV');
+
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpressuser' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'Yanjiuai123.' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+. . .
+
+define('FS_METHOD', 'direct');
